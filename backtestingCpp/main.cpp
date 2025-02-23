47a6e9ac48a6f8d74ad410890b9e8f1af0550b25
oigre <sergiomarreromarrero@gmail.com>
2025-02-23 20:34:33 +0000

    - add cpp project

---
+++ backtestingCpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <hdf5.h>
+
+
+
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    std::cout << "Hello World!\n";
+    hid_t fapl = H5Pcreate(H5P_FILE_ACCESS);
+
+}
+
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
